e8dad373168b4f30f571ddff42d64347d9fe8cc9
Ludvig Isaksson <ludvig.isaksson@gmail.com>
2018-04-01 15:18:31 +0200

    basic project structure

---
+++ Game-Test/Assets/Scripts/Abilities.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilities : MonoBehaviour {
+
+    public float RecallCooldown = 5.0f;
+    private float RecallTimer;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RecallTimer -= Time.deltaTime;
+        if (Input.GetKey("e") && RecallTimer <= 0)
+        {
+            Debug.Log("Recall");
+            RecallTimer = RecallCooldown;
+        }
+		
+	}
+}
---
+++ Game-Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+	}
+}
