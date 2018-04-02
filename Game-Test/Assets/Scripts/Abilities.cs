898194f37423caf0350ac9a70fed6497adbd387b
Ludvigis <Ludvig.isaksson@gmail.com>
2018-04-02 10:33:05 +0200

    basic raycast

--- Game-Test/Assets/Scripts/Abilities.cs
+++ Game-Test/Assets/Scripts/Abilities.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class Abilities : MonoBehaviour {
 
     public float RecallCooldown = 5.0f;
+    public Camera cam;
     private float RecallTimer;
 
+    public float weaponRange;
+    public float weaponAccuracy;
+
+    
+    
 	// Use this for initialization
 	void Start () {
        
@@ -20,6 +26,25 @@ public class Abilities : MonoBehaviour {
             Debug.Log("Recall");
             RecallTimer = RecallCooldown;
         }
+        if (Input.GetMouseButton(0))
+        {
+            Fire();
+        }
 		
 	}
+
+    private void Fire()
+    {
+        float spreadX = Random.Range(-(1 - weaponAccuracy), 1 - weaponAccuracy);
+        float spreadY = Random.Range(-(1 - weaponAccuracy), 1 - weaponAccuracy);
+        Vector3 bulletSpread = new Vector3(spreadX, spreadY, 0);
+        Vector3 targetDirection = cam.transform.forward + bulletSpread;
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, targetDirection, out hit, weaponRange))
+        {
+            Debug.Log(hit.transform.name);
+        }
+        Debug.DrawRay(cam.transform.position, targetDirection * weaponRange, Color.red,1);
+        
+    }
 }
--- Game-Test/Assets/Scripts/PlayerMovement.cs
+++ Game-Test/Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float moveSpeed = 5.0f;
+    public float mouseSensitivity = 1.0f;
+    public Camera cam;
 
-    private Rigidbody rb;
+    private CharacterController controller;
 
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        rb.AddForce(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
-	}
+
+        //TODO time.deltaTime på rotation?
+        float rotX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float rotY = - (Input.GetAxis("Mouse Y") * mouseSensitivity);
+        transform.Rotate(new Vector3(0, rotX, 0));
+        cam.transform.Rotate(rotY, 0, 0);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed);
+        movement = transform.rotation * movement;
+        controller.Move(movement * Time.deltaTime);
+
+    }
 }
